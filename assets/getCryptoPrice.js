115f3e1aeb34638c6a389b2ea005d6c5da373e2f
aaron <aaronyanes92@gmail.com>
2020-11-01 16:45:15 -0800

    added api call for prices

--- assets/getCryptoPrice.js
+++ assets/getCryptoPrice.js
@@ -1,35 +1,25 @@
 const axios = require('axios')
 
-const price = document.querySelector('h1')
-const priceETH = document.getElementById('priceETH');
-const priceLTC = document.getElementById('priceLTC');
+const name = document.getElementById('name');
+const img = document.getElementById('image');
+const price = document.getElementById('price');
+const topTenCrypto = ['BTC', 'ETH', 'LINK', 'BCH', 'LTC', 'XRP', 'TRX', 'EOS', 'BNB', 'BSV'];
+const imageArray = [];
 
-const targetPrice = document.getElementById('targetPrice');
-const targetPriceETH = document.getElementById('targetPriceETH');
-const targetPriceLTC = document.getElementById('targetPriceLTC');
+//TODO : GET IMAGES ARRAY
+//       SHOW ALL 10 CURRENCIES (CURRENTLY ONLY SHOWS ONE)
+//       ADD CSSS
 
-// TODO: make this render new list every time its called
-const getCryptoPrice = crypto => {
-    axios.get(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${crypto}&tsyms=USD`)
+(function getTopTenCryptos() {
+    for (const coin of topTenCrypto) {
+        axios.get(`https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD`)
         .then(res => {
-            const currency = res.data.crypto.USD;
-            price.innerHTML = `$ ${cryptos.toLocaleString('en')}` //replacing the h1 with the current BTC price
-
-            //creating the notification
-            if(targetPrice.innerHTML != '' && targetPriceVal < currency) {
-                // const myNotification = new window.Notification(notification.title, notification);
-                notifier.notify({
-                    title: `${crypto} Alert`,
-                    message: `${crypto} just beat your target price!`,
-                    icon: path.join(__dirname, '../assets/icons/btc.png'),
-                    sound: true,
-                    wait: false
-                });
-            }
+           console.log(res);
+           name.innerHTML = coin;
+           price.innerHTML = res.data.USD;
         })
         .catch(err => {
             console.error(err);
         })
-}
-
-module.exports = getCryptoPrice;
+    }
+})()
